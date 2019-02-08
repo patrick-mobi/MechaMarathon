64c1fd7bc6cda8512c6d01411e01aa82e5470ba8
patrick-mobi <patrick_mohr@gmx.net>
2019-02-08 13:08:26 +0100

    some commenting around

---
+++ mm.py
@@ -0,0 +1,20 @@
+from tkinter import *
+root = Tk()
+
+class Robot
+    self.postion = [1,1]
+    self.direction = "n")
+    self.tokens = 0
+    self.energy = 5
+    self.registers = ["","","","",""]
+    self.deck = ["","","",""'''etc, etc''']
+    self.discard = []
+    self.hand = []
+
+
+
+#if deck is empty, take discard list, shuffle, and make new deck list
+
+root.mainloop()
+
+
