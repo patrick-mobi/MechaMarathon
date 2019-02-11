f0a688472a2b8f07b5f57fb1130a52563e325bd2
patrick-mobi <patrick_mohr@gmx.net>
2019-02-11 08:27:50 +0100

    graphics

--- mm.py
+++ mm.py
@@ -4,12 +4,16 @@ root = Tk()
 def test():
     i=1
 
-#counts drawn squares
-squareCount = 0
+#variables for images
+neutral = PhotoImage(file="graphics/neutral.gif")
+r1n =     PhotoImage(file="graphics/r1n.gif")
+r1e =     PhotoImage(file="graphics/r1e.gif")
+r1s =     PhotoImage(file="graphics/r1s.gif")
+r1w =     PhotoImage(file="graphics/r1w.gif")
 
-class robot:
+class robot():
     def __init__(self):
-        self.postion = [1,1]
+        self.postion = [0,0]
         self.direction = "n"
         self.tokens = 0
         self.energy = 5
@@ -109,29 +113,76 @@ class robot:
         else:
             print ("mistake at def move(reg)")
 
-
-def drawSquare(row,column):
-    global squareCount
+#create r1
+r1=robot
     
 
     
 #define all squares
-neutral1=Label(width=3, height=3, text="[ ]")
-neutral2=Label(width=3, height=3, text="[ ]")
-neutral3=Label(width=3, height=3, text="[ ]")
-neutral4=Label(width=3, height=3, text="[ ]")
-test=Button(command=test(), width=1)
+l00=Label(width=30, height=30, image=neutral)
+l10=Label(width=30, height=30, image=neutral)
+l20=Label(width=30, height=30, image=neutral)
+l30=Label(width=30, height=30, image=neutral)
+l40=Label(width=30, height=30, image=neutral)
+l01=Label(width=30, height=30, image=neutral)
+l11=Label(width=30, height=30, image=neutral)
+l21=Label(width=30, height=30, image=neutral)
+l31=Label(width=30, height=30, image=neutral)
+l41=Label(width=30, height=30, image=neutral)
+l02=Label(width=30, height=30, image=neutral)
+l12=Label(width=30, height=30, image=neutral)
+l22=Label(width=30, height=30, image=neutral)
+l32=Label(width=30, height=30, image=neutral)
+l42=Label(width=30, height=30, image=neutral)
+l03=Label(width=30, height=30, image=neutral)
+l13=Label(width=30, height=30, image=neutral)
+l23=Label(width=30, height=30, image=neutral)
+l33=Label(width=30, height=30, image=neutral)
+l43=Label(width=30, height=30, image=neutral)
+l04=Label(width=30, height=30, image=neutral)
+l14=Label(width=30, height=30, image=neutral)
+l24=Label(width=30, height=30, image=neutral)
+l34=Label(width=30, height=30, image=neutral)
+l44=Label(width=30, height=30, image=neutral)
+
+lr1n=Label(width=30, height=30, image=r1n)
+
+b1for=Button(command=test(), width=0, text="1for")
 
 #draw all squares
 def drawBoard():
-    global squareCount
-    squareCount = 0
-    neutral1.grid(row=0,column=0)
-    neutral2.grid(row=0,column=1)
-    neutral3.grid(row=1,column=0)
-    neutral4.grid(row=1,column=1)
+    #draw empty board
+    l00.grid(row=0,column=0)
+    l10.grid(row=1,column=0)
+    l20.grid(row=2,column=0)
+    l30.grid(row=3,column=0)
+    l40.grid(row=4,column=0)
+    l01.grid(row=0,column=1)
+    l11.grid(row=1,column=1)
+    l21.grid(row=2,column=1)
+    l31.grid(row=3,column=1)
+    l41.grid(row=4,column=1)
+    l02.grid(row=0,column=2)
+    l12.grid(row=1,column=2)
+    l22.grid(row=2,column=2)
+    l32.grid(row=3,column=2)
+    l42.grid(row=4,column=2)
+    l03.grid(row=0,column=3)
+    l13.grid(row=1,column=3)
+    l23.grid(row=2,column=3)
+    l33.grid(row=3,column=3)
+    l43.grid(row=4,column=3)
+    l04.grid(row=0,column=4)
+    l14.grid(row=1,column=4)
+    l24.grid(row=2,column=4)
+    l34.grid(row=3,column=4)
+    l44.grid(row=4,column=4)
+
+    #draw robots on board
+    lr1n.grid(row=0,column=0)
     
-    test.grid(row=3,column=3)
+    #draw buttons
+    b1for.grid(row=0,column=5)
 
 
 
