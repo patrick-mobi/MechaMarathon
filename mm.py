7dfbbcd7d00f3394bc58d7d7a386a5bebfa4860e
patrick-mobi <patrick_mohr@gmx.net>
2019-02-12 18:45:00 +0100

    robots are now smaller so the rest of the square can be seen

--- mm.py
+++ mm.py
@@ -26,20 +26,16 @@ testMap=[
 ["neutral","neutral","neutral","neutral","neutral","neutral","neutral","neutral","neutral","neutral"]]
 
 
+#victorytokens, list of 2xposition
+#v0 = []
+
+#energycubes, list of 2xposition
+#e0 = []
+
 
 #for loading in of later, custom maps
 board=testMap
 
-#variables for images
-neutral = PhotoImage(file="graphics/neutral.gif")
-r1n =     PhotoImage(file="graphics/r1n.gif")
-r1e =     PhotoImage(file="graphics/r1e.gif")
-r1s =     PhotoImage(file="graphics/r1s.gif")
-r1w =     PhotoImage(file="graphics/r1w.gif")
-fastconn= PhotoImage(file="graphics/fastconn.gif")
-fastcone= PhotoImage(file="graphics/fastcone.gif")
-fastcons= PhotoImage(file="graphics/fastcons.gif")
-fastconw= PhotoImage(file="graphics/fastconw.gif")
 
 #robots, list of
 #2xposition, direction, victorytokens, energy, register, deck, discard, hand
@@ -60,11 +56,63 @@ r7 = [7,0,"e",0,5,["","","","",""],["1for","1for","1for","1for","1for","2for","2
 r8 = [8,0,"e",0,5,["","","","",""],["1for","1for","1for","1for","1for","2for","2for","2for","3for","1back","left","left","left","right","right","right","uturn","again","again","powerup"], [], []]
 
 
-#victorytokens, list of 2xposition
-#v0 = []
 
-#energycubes, list of 2xposition
-#e0 = []
+
+
+
+
+
+#variables for images
+neutral = PhotoImage(file="graphics/neutral.gif")
+r1n =     PhotoImage(file="graphics/r1n.gif")
+r1e =     PhotoImage(file="graphics/r1e.gif")
+r1s =     PhotoImage(file="graphics/r1s.gif")
+r1w =     PhotoImage(file="graphics/r1w.gif")
+
+r2n =     PhotoImage(file="graphics/r2n.gif")
+r2e =     PhotoImage(file="graphics/r2e.gif")
+r2s =     PhotoImage(file="graphics/r2s.gif")
+r2w =     PhotoImage(file="graphics/r2w.gif")
+
+r3n =     PhotoImage(file="graphics/r3n.gif")
+r3e =     PhotoImage(file="graphics/r3e.gif")
+r3s =     PhotoImage(file="graphics/r3s.gif")
+r3w =     PhotoImage(file="graphics/r3w.gif")
+
+r4n =     PhotoImage(file="graphics/r4n.gif")
+r4e =     PhotoImage(file="graphics/r4e.gif")
+r4s =     PhotoImage(file="graphics/r4s.gif")
+r4w =     PhotoImage(file="graphics/r4w.gif")
+
+r5n =     PhotoImage(file="graphics/r5n.gif")
+r5e =     PhotoImage(file="graphics/r5e.gif")
+r5s =     PhotoImage(file="graphics/r5s.gif")
+r5w =     PhotoImage(file="graphics/r5w.gif")
+
+r6n =     PhotoImage(file="graphics/r6n.gif")
+r6e =     PhotoImage(file="graphics/r6e.gif")
+r6s =     PhotoImage(file="graphics/r6s.gif")
+r6w =     PhotoImage(file="graphics/r6w.gif")
+
+r7n =     PhotoImage(file="graphics/r7n.gif")
+r7e =     PhotoImage(file="graphics/r7e.gif")
+r7s =     PhotoImage(file="graphics/r7s.gif")
+r7w =     PhotoImage(file="graphics/r7w.gif")
+
+r8n =     PhotoImage(file="graphics/r8n.gif")
+r8e =     PhotoImage(file="graphics/r8e.gif")
+r8s =     PhotoImage(file="graphics/r8s.gif")
+r8w =     PhotoImage(file="graphics/r8w.gif")
+
+fastconn= PhotoImage(file="graphics/fastconn.gif")
+fastcone= PhotoImage(file="graphics/fastcone.gif")
+fastcons= PhotoImage(file="graphics/fastcons.gif")
+fastconw= PhotoImage(file="graphics/fastconw.gif")
+
+
+
+
+
 
 
 
@@ -82,21 +130,91 @@ def square(type):
     if type == "fastcone":
         return fastcone
 
-def cursor(d):
-    if d == "n":
-        return "^"
-    if d == "e":
-        return ">"
-    if d == "s":
-        return "v"
-    if d == "w":
-        return "<"
 
-def drawBoard():
-    global r1, neutral
-    global fastconn, fastcone, fastcons, fastconw
-    
+def r1cursor(d):
+    if d == "n":
+        return r1n
+    if d == "e":
+        return r1e
+    if d == "s":
+        return r1s
+    if d == "w":
+        return r1w
+
+def r2cursor(d):
+    if d == "n":
+        return r2n
+    if d == "e":
+        return r2e
+    if d == "s":
+        return r2s
+    if d == "w":
+        return r2w
+
+def r3cursor(d):
+    if d == "n":
+        return r3n
+    if d == "e":
+        return r3e
+    if d == "s":
+        return r3s
+    if d == "w":
+        return r3w
+
+def r4cursor(d):
+    if d == "n":
+        return r4n
+    if d == "e":
+        return r4e
+    if d == "s":
+        return r4s
+    if d == "w":
+        return r4w
+
+def r5cursor(d):
+    if d == "n":
+        return r5n
+    if d == "e":
+        return r5e
+    if d == "s":
+        return r5s
+    if d == "w":
+        return r5w
+
+def r6cursor(d):
+    if d == "n":
+        return r6n
+    if d == "e":
+        return r6e
+    if d == "s":
+        return r6s
+    if d == "w":
+        return r6w
+
+def r7cursor(d):
+    if d == "n":
+        return r7n
+    if d == "e":
+        return r7e
+    if d == "s":
+        return r7s
+    if d == "w":
+        return r7w
+
+def r8cursor(d):
+    if d == "n":
+        return r8n
+    if d == "e":
+        return r8e
+    if d == "s":
+        return r8s
+    if d == "w":
+        return r8w
+
+
+
 #draw empty board
+def drawBoard():
     x=0
     while x < len(board):
         y=0
@@ -106,14 +224,15 @@ def drawBoard():
         x+=1
 
     #draw robots
-    Label(root, text=cursor(r1[2]), fg="red").grid(row=r1[0], column=r1[1])
-    Label(root, text=cursor(r2[2]), fg="blue").grid(row=r2[0], column=r2[1])
-    Label(root, text=cursor(r3[2]), fg="green").grid(row=r3[0], column=r3[1])
-    Label(root, text=cursor(r4[2]), fg="yellow").grid(row=r4[0], column=r4[1])
-    Label(root, text=cursor(r5[2]), fg="purple").grid(row=r5[0], column=r5[1])
-    Label(root, text=cursor(r6[2]), fg="dark orange").grid(row=r6[0], column=r6[1])
-    Label(root, text=cursor(r7[2]), fg="white").grid(row=r7[0], column=r7[1])
-    Label(root, text=cursor(r8[2]), fg="black").grid(row=r8[0], column=r8[1])
+    Label(root, image=r1cursor(r1[2])).grid(row=r1[0], column=r1[1])
+    Label(root, image=r2cursor(r1[2])).grid(row=r2[0], column=r2[1])
+    Label(root, image=r3cursor(r1[2])).grid(row=r3[0], column=r3[1])
+    Label(root, image=r4cursor(r1[2])).grid(row=r4[0], column=r4[1])
+    Label(root, image=r5cursor(r1[2])).grid(row=r5[0], column=r5[1])
+    Label(root, image=r6cursor(r1[2])).grid(row=r6[0], column=r6[1])
+    Label(root, image=r7cursor(r1[2])).grid(row=r7[0], column=r7[1])
+    Label(root, image=r8cursor(r1[2])).grid(row=r8[0], column=r8[1])
+
 
     #victorytokens
     #draw energycubes
